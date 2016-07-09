c3de583b86f68210feef2243a319c2a0c00589d9
Lazini <Lazini@users.noreply.github.com>
2016-07-09 16:49:33 +0300

    Create Main.java

---
+++ lazini/apps/chunkdeleter/Main.java
@@ -0,0 +1,17 @@
+package lazini.apps.chunkdeleter;
+
+import java.io.File;
+
+public class Main {
+
+//	public static boolean running = true;
+
+	private File mcaDir;
+
+	public static void main(String[] args) {
+		GUI gui = new GUI();
+//		File mcaDir = new File(gui.getLocation().getText(), "region");
+	}
+
+
+}
